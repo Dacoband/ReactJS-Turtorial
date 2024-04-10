0f3150d17ccf70d48c0f1d74ea91efbcfd9e6763
Huỳnh Thiện Nhân <bolicious123@gmail.com>
2024-04-10 19:43:33 +0700

    How Create Project

---
+++ demo-react/src/App.jsx
@@ -0,0 +1,6 @@
+// Component have Functional Component - Create a new Component instance with the given parameters and options and return 
+function DemoReact(options) {
+  return <h1>ReactJS-Turtorial</h1>
+}
+
+export default DemoReact;
---
+++ demo-react/src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import DemoReact from './App.jsx'
+import './index.css'
+
+// Single Page Application
+// Client Side Rendering
+
+//document.getElementById("root") : Find tag have "root" property
+ReactDOM.createRoot(document.getElementById('root')).render( <DemoReact />);
